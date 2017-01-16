f7229a6776d3d194665582b07c01a6d1173cdb8e
Bruno Azevedo <bruno.azevedo.brg94@gmail.com>
2017-01-16 23:02:43 +0000

    Some changes on blast.py

--- blast.py
+++ blast.py
@@ -6,6 +6,7 @@ Created on Mon Dec 22 11:59:58 2014
 """
 
 import os
+import sys
 
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
@@ -15,28 +16,66 @@ from Bio.Blast import NCBIXML
 import seq
 
 
-indice = 0
-protein_record = {}
-protein_record["Sequence"] = []
-for x in seq.features_gene["Locus_tag"]:
-    seq_protein = Seq(str(seq.features_protein["Sequence"][indice]), IUPAC.extended_protein)
-    protein_record["Sequence"].append(SeqRecord(seq_protein))
-    indice += 1
 
 if not os.path.exists("Blast"):
     os.makedirs("Blast")
 
+print "Deseja fazer blast de todas as proteínas ou só de uma?"
+print " 1 -> Uma"
+print " 2 -> Todas"
 
-indice = 0
-for gene in seq.features_gene["Locus_tag"]:
-    print seq.features_gene["Locus_tag"][indice]
-    save_file = open('./Blast/blast_%s.xml' %gene, 'w')
-    handler = NCBIWWW.qblast('blastp','swisprot',protein_record["Sequence"][indice].format('gb'))
-    save_file.write(handler.read())
-    save_file.write('\n\n')
-    save_file.close()
+answer = raw_input("Resposta: ")
+
+if answer == "1" or answer == "Uma" or answer == "UMA" or answer == "uma":
+    gene = raw_input("Introduza o locus_tag do gene a pesquisar: ")
+    error = -1
+    aux = 0
+    """
+        Get Seq
+    """
+    for x in seq.features_gene["Locus_tag"]:
+        if x == gene:
+            seq_protein = Seq(str(seq.features_protein["Sequence"][aux]), IUPAC.extended_protein)
+            #protein found, no error
+            error = 0
+            break
+        aux += 1
+    #protein not found
+    if error == -1:
+        sys.exit("Locus_tag não encontrado")
+
+    """
+        Run blast
+    """
+    ofile = open('./Blast/blast_%s.xml' %gene, 'w')
+    print "A executar o blast"
+    handler = NCBIWWW.qblast('blastp','swisprot',seq.features_protein["Sequence"][aux].format('gb'))
+    ofile.write(handler.read())
+    ofile.write('\n\n')
+    ofile.close()
     handler.close()
-    indice += 1
+
+
+
+else:
+    indice = 0
+    protein_record = {}
+    protein_record["Sequence"] = []
+    for x in seq.features_gene["Locus_tag"]:
+        seq_protein = Seq(str(seq.features_protein["Sequence"][indice]), IUPAC.extended_protein)
+        protein_record["Sequence"].append(SeqRecord(seq_protein))
+        indice += 1
+
+    indice = 0
+    for gene in seq.features_gene["Locus_tag"]:
+        print "A executar o blast para: " + seq.features_gene["Locus_tag"][indice]
+        save_file = open('./Blast/blast_%s.xml' %gene, 'w')
+        handler = NCBIWWW.qblast('blastp','swisprot',protein_record["Sequence"][indice].format('gb'))
+        save_file.write(handler.read())
+        save_file.write('\n\n')
+        save_file.close()
+        handler.close()
+        indice += 1
 """
 verify = open('./Blast/blast_%s_verification.txt' %gene, 'w')
 E_VALUE_THRESH = 0.05
